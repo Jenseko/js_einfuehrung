b9f395f9c40f89919c769b57cafdc1c322a30e25
Jenseko <jenscode23@gmail.com>
2023-02-16 15:27:40 +0100

    alle Übungen in einem File

---
+++ assets/js/main.js
@@ -0,0 +1,123 @@
+// // lvl_1-1: console.log ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+
+// let a = 12;
+// let b = a*3;
+// console.log(b);
+
+// // document.write(b)
+
+// console.log("Anton");
+
+// let firstName = "Anton";
+// console.log(firstName);
+
+// let age = 28;
+// console.log(age);
+
+// lvl_1-4: popup boxes ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+// window.alert("Hallo Welt");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein." , "Cancel");
+
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+
+// let alter = window.prompt("Gib mir bitte dein Alter.");
+
+// console.log(alter);
+
+// let b = 5;
+// let a = b * 5-3
+// window.alert(a);
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt: ";
+// let groesse = "2,72m";
+// window.confirm(mann + groesse);
+
+// lvl_1-5: variables ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+// let carName = "BMW";
+// console.log(carName);
+
+// let x = 150;
+// console.log(x);
+
+// let y = 50;
+// console.log(y);
+
+// let z = 'x+y';
+// console.log(x + y);
+
+// let firstName = 'John ';
+// let lastName = 'Doe ';
+// let age = 35;
+// console.log(firstName + lastName + age);
+
+// lvl_1-6: Let Const ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+// let x = 34;
+// let x;
+// let x = 67;
+
+// console.log(x);
+
+// const y = 34;
+// const y;
+// const y = 67;
+// console.log(y);
+
+// // lvl_1-7: Coding Conventions ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+// let helloWorld = 'Hello World';
+// const pi = 3.14;
+// const burjdubai = "828m";
+// let fullname = 'Jan Schmidt';
+// const eifeltower = '324m';
+// let kursstatus = 'true';
+
+
+// lvl_1-12: Arithmetische Operationen ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+// let score = 5 + 5 *10;
+// console.log('Ergebnis = ' + score);
+
+// let score = (5 + 5) *10;
+// console.log('Ergebnis = '+ score);
+
+// let score = 0;
+// score = score + 10;
+// console.log('Ergebnis = ' + score);
+
+// let zahl = 1;
+// zahl = zahl + 1;
+// zahl += 1;
+
+// console.log(zahl++);
+// zahl++;
+// console.log(zahl);
+
+// zahl--;
+// console.log(zahl);
+
+
+// lvl_1-13: Arithmetische Wiederholung ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+// // Addition
+// let addition_operator = 1+1;
+// console.log("in addition = " + addition_operator);
+
+// // Subtraktion
+// let subtraktion_operator = 2-1;
+// console.log("subtraktion: " + subtraktion_operator);
+
+
+// // Division
+// let division_operator = 4/2;
+// console.log("division: " + division_operator);
+
+// // Modul
+// let modulus_operator = 14 % 5;
+// console.log ("modulus: " + modulus_operator);
